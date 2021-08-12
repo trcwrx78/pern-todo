2f0c7b7680d243ac0d1539a2377180969a686d1f
Torrence Cole <trcole@gmail.com>
2021-08-12 09:19:22 -0400

    added comments about db connection methods

--- db/index.js
+++ db/index.js
@@ -1,3 +1,4 @@
+// PostgreSQL Heroku setup
 const { Client } = require('pg');
 
 const client = new Client({
@@ -12,3 +13,12 @@ client.connect();
 module.exports = {
   query: (text, params) => client.query(text, params),
 };
+
+// Development Local setup
+// const { Pool } = require('pg');
+
+// const pool = new Pool();
+
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
